197c355a82a481f0dbb7bc93e9a0abbeca23eccf
eunjegolem <ppoppos@hotmail.com>
2025-03-20 12:43:56 +0900

    fast patch
    빠르게 기능 추가!!

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Git을 공부해보자!!");
+            Console.WriteLine("새로운 기능 추가!!");
         }
     }
 }
